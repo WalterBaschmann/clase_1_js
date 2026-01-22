b6bb05ddb326d9f59d935bf88f47be0ba9ea9876
Walter Baschmann <baschmann.w@gmail.com>
2026-01-21 21:49:02 -0300

    practica en js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,11 @@ let apellido = 'Faccio'
 console.log(apellido)
 //literal string
 console.log(`Tu nombre es: ${nombre} ${apellido} y tu edad es: ${edad}`)
+
+let number1 = 25
+let number2 = 75
+let suma = number1 + number2
+console.log(suma)
+
+let number3 = prompt('dame un numero')
+console.log(number3)
