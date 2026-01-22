c84c206d354ba821354168464e7c2e10739415dc
Walter Baschmann <baschmann.w@gmail.com>
2026-01-21 21:26:20 -0300

    practica en js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//variables
+
+let nombre = 'david'
+console.log(nombre)
+
+const edad = 25
+
+let isVerdadero = true
+
+let apellido = 'Faccio'
+console.log(apellido)
+//literal string
+console.log(`Tu nombre es: ${nombre} ${apellido} y tu edad es: ${edad}`)
